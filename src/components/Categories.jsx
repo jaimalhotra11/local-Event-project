f3e8b06b293b556ffe0767ae7f5eb8d9b251cc62
surendraranwa <surendraranwa20@gmail.com>
2025-01-09 22:05:09 +0530

    Commit add json to category section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,50 +1,25 @@
-import './Categories.css'
-import image1 from '../assets/images/1.jpg'
+import "./Categories.css";
+import CategoriesJson from "../JsonData/Categories.json";
 
 function Categories() {
   return (
     <>
-    <div className='cat-parent'>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        <div className='cat-child'>
-            <div className='cat-img2'>
-                <img src={image1} className='cat-img'/>
-            </div>
-            <p className='cat-para'>pottery</p>
-        </div>
-        
-    </div>
+      <div className="cat-parent">
+        {CategoriesJson.map((item, i) => {
+          return (
+            <>
+              <div className="cat-child" key={i}>
+                <div className="cat-img2">
+                  <img src={item?.image} className="cat-img" />
+                </div>
+                <p className="cat-para">{item?.category}</p>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
